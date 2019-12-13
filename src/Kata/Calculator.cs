70685ec5647b1b8f4d1c9d838eecb3777f40104d
Carlos Gochez <cgotcha@gmail.com>
2019-12-13 10:08:36 -0700

    Adding when_adding_n_numbers and make it fail for the right reason

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Kata
 {
     public class Calculator
@@ -6,7 +8,17 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(val)) return 0;
 
-            return 7;
+            if (val.Length == 1)
+                return Int32.Parse(val[0]+"");
+                                
+            var vals = val.Split(",");
+            int sum = 0;
+            for(int x=0; x < 2; x++)
+            {
+                sum += Int32.Parse(vals[x]);
+            }
+
+            return sum;
         }
     }
 }
